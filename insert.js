1a1f8cba5a5b645f73da726491c1c453927463ba
aduno <aduno7647@gmail.com>
2023-04-05 20:53:00 -0400

    Set up api for user registration, booking and rent insertion

--- insert.js
+++ insert.js
@@ -8,45 +8,92 @@ const { runQuery } = require('./database');
 function format(){
     var formatted = "("
     for(var i=0; i<arguments.length-1;i++){
-        formatted+=arguments[i] +',';
+        if(arguments[i]){
+            formatted+=arguments[i] +',';
+        }
     }
     formatted+= (formatted[arguments.length-1]+")")
     return formatted;
 }
 
-// Add a booking (Automatically create a copy of this data in the archive)
-router.get('/book',(req,res)=>{
+// Add a booking 
+router.post('/book',(req,res)=>{
     // MAKE A SEPARATE FUNCTION FOR THIS AND IN THE MAIN FUNCTION HERE, CHECK THAT BOTH PROMISES SUCCESSFULLY FINISHES
     var values = format(req.query.hotelID, req.query.roomNumber, req.query.customerID, req.query.start, req.query.end);
     var insert = 'insert into booking(hotel_id, room_number, customer_id, booking_start_date, booking_end_date) values '+ values;
     var response = runQuery(insert);
     response.
-        then((result) => console.log("Successfully created booking")).
-        catch((err) => console.log("Failed to create booking"));
-
-    // Archive
-    // FIX THIS MAKE A SEPARATE FUNCTION FOR THIS
-    var chainNameQuery = "select chain_name from hotel where hotel_id='"+hotelID+"'"
-    response = runQuery(chainNameQuery);
-    response.
         then((result) => {
-            values= values.substring(0, values.length-2)+result[0]['chain_name']+")"
-            insert = 'insert into booking_archive(hotel_id, room_number, customer_id, booking_start_date, booking_end_date, chain_name) values '+ values
-        })
-        .catch((err) => console.log("Failed to archive"));
+            console.log("Successfully created booking");
+            res.send(result);
+        }).
+        catch((err) => {
+            console.log("Failed to create booking");
+            res.sendStatus(500);
+        });
 });
 
 // Create a check-in + check-in from booking (+Archive)
+router.post('/rent', (req, res) => {
+    var values = format(req.query.customer_id,req.query.hotel_id, req.query.room_number, req.query.start, req.query.end, req.query.booking_id);
+    if(req.query.booking_id){
+        var insert = 'insert into booking(customer_id, hotel_id, room_number, check_in_date, check_out_date, booking_id) values '+ values;
+    }
+    else{
+        var insert = 'insert into booking(customer_id, hotel_id, room_number, check_in_date, check_out_date) values '+ values;
+    }
+    runQuery(insert)
+        .then((result)=>{
+            console.log("Successfully created renting")
+            res.send(result);
+        })
+        .catch((err)=>{
+            res.sendStatus(500);
+        })
 
+})
 // Create new hotel
-
 // Create new rooms
-
 // Create new hotel chain
 
 // Creating new user
+// Check what happens if the user dose not enter unit number
 router.post('/user', (req, res) => {
-    var query = "insert into customer(first_name, last_name, country) values ('"+req.body.username+"";
+    const date = new Date();
+    let currentDay= String(date.getDate()).padStart(2, '0');
+    let currentMonth = String(date.getMonth()+1).padStart(2,"0");
+    let currentYear = date.getFullYear();
+    let currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+    var values = formatted(req.query.first_name, req.query.last_name,
+        req.query.phone_number, req.query.email, req.query.country,
+        req.query.city, req.query.street_name, req.query.unit_number,
+        req.query.sin, currentDate);
+    if(req.query.unit_number){
+        var query = `
+        insert into customer(first_name, last_name, 
+            phone_number, email, address_country, address_city,
+            address_street_name, address_unit_number, ssn_sin,
+            registration_date) 
+            values ` + values;
+    }
+    else{
+        var query = `
+        insert into customer(first_name, last_name, 
+            phone_number, email, address_country, address_city,
+            address_street_name, ssn_sin,
+            registration_date) 
+            values ` + values;
+    }
+    
+    runQuery(query)
+        .then(result=>{
+            console.log("Successfully created user");
+            res.sendStatus(200);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.sendStatus(500);
+        })
 })
 
 
